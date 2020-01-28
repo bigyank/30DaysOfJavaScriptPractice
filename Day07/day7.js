4ca606545e3e6d2d4daa74ac5f3a78466285e908
BlueOrange <this.is.blueorange@gmail.com>
2020-01-28 11:31:47 +0545

    part 3 begin

--- Day07/day7.js
+++ Day07/day7.js
@@ -36,3 +36,60 @@ function seasonFinder(monthNum) {
 }
 const season = seasonFinder(12);
 console.log(season);
+
+// part 2
+// takes array as a parameter and it prints out each value of the array
+
+function prntArr(arr) {
+    console.log(...arr);
+}
+
+prntArr([1, 2, 'a', 12]);
+
+//  showDateTime which shows time in this format: 08/01/2020 04:08 using the Date object.
+function formatedDate() {
+    const dateTime = new Date();
+    const formDate = `${dateTime.getDate()}/${dateTime.getMonth() +
+        1}/${dateTime.getFullYear()} ${dateTime.getHours()}:${dateTime.getMinutes()}`;
+    return formDate;
+}
+
+const dateToday = formatedDate();
+console.log(dateToday);
+
+// swapValues. This function swaps value of x to y.
+
+function swapValues(x, y) {
+    [x, y] = [y, x];
+    return `${x} is a x and ${y} is a y now`;
+}
+
+const swpdVal = swapValues(12, 8);
+console.log(swpdVal);
+
+// takes any number of arguments and return the sum of the arguments
+
+function sumArgs(...numbers) {
+    return numbers.reduce((sum, number) => sum + number, 0);
+}
+
+const sumArgsVal = sumArgs(5, 2, 2);
+console.log(sumArgsVal);
+
+// random word gen
+function randWord() {
+    return Math.random()
+        .toString(36)
+        .slice(2, 7)
+        .toUpperCase();
+}
+
+console.log(randWord());
+
+// random Hex generator
+function hexGen() {
+    const decNum = Math.floor(0x1000000 * Math.random());
+    const hexNum = decNum.toString(16);
+    return hexNum;
+}
+console.log(hexGen(100));
